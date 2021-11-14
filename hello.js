3f6f3da9ac3d33491fbb791f743ddd5de9c7310b
Moyoummu <maxbalaklf@gmail.com>
2021-11-14 20:16:44 +0300

    homework4 done

---
+++ hello.js
@@ -0,0 +1,31 @@
+'use strict';
+function sayHi()
+{
+    console.log('Hello, world!');
+}
+sayHi();
+{
+    let x = 5;
+    var y = 10;
+}
+let x = 6;
+console.log(x,y);
+let mult = (a,b) => a*b;
+console.log('Arrow function: ', mult(x,y));
+console.log('(x=6) === 6 comparison:\n',x === 6);
+console.log('(x=6) == 6 comparison:\n',x == 6);
+const arr = [1, 3, "pepe", y];
+console.log('typeof arr:\n', typeof(arr));
+console.log('for of loop prints the array:\n');
+for (let i of arr)
+{
+    console.log(i);
+}
+globalThis["yep"] = 59;
+let yes = "yes", nay = "nay";
+console.log('before deconstructuction: ', nay, yes);
+[nay, yes] = [yes,nay];
+console.log('after: ', nay, yes);
+let jej = 10.895;
+console.log('before truncation: ', jej);
+console.log('after: ', Math.trunc(jej));
